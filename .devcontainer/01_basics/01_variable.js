fc93ed4bdf0b82bc76902824caae513637b3ea15
ria123-ship <riya1singla@gmail.com>
2025-03-14 06:36:33 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const amountID = 1234
+let accountEmail ="riya@gmail.com"
+var accountPassword ="12345"
+let accountstate;
+//console.log(amountID);
+/*
+perfer not use var
+because of issue in block and functional scope 
+*/
+console.table([accountEmail,accountPassword,amountID,accountstate])
---
+++ .devcontainer/01_basics/02_Datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // we are using js code as never version
+//alert(3+3)// we are using nodejs,not browser
+console.log(3+3) // code read easily
+console.log("riya");
+let name = "riya"
+let age = 21
+let isLoggedIn = false
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null=> standlone value
+// undefined =>
+//symbol=> unique
+//object 
+console.log(typeof undefined);
