5ea1b0e03c9ed930f21d1fd704e0c936174f4299
ria123-ship <riya1singla@gmail.com>
2025-03-15 07:59:02 +0000

    summary

---
+++ .devcontainer/01_basics/summary.js
@@ -0,0 +1,17 @@
+let a = null;
+let b;
+const c = true;
+let d = 123;
+let e = "ab";
+ let r = {
+    name : "s"
+ }
+ /*console.table([ typeof "a",a,"b", b , c,d,e,r]);
+ Undefined	"undefined"
+Null	"object"
+Boolean	"boolean"
+Number	"number"
+String	"string"
+Object (native and does not implement [[Call]])	"object"
+Object (native or host and does implement [[Call]])	"function"
+Object (host and does not implement [[Call]])	Implementation-defined except may not be "undefined", "boolean", "number", or "string".*/
