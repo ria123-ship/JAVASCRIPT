e5222c48cd37470fe644c4fabcf937e50891b3c1
ria123-ship <riya1singla@gmail.com>
2025-03-19 14:37:08 +0000

    math nd num

---
+++ .devcontainer/01_basics/number and math.js
@@ -0,0 +1,21 @@
+//const score = 400
+//console.log(score)
+//const balance = new Number(100.89)
+//console.log(balance)
+//console.log(balance.toFixed(1));
+//console.log(balance.toPrecision(1));
+//const hunderad = 100000000
+//console.log(hunderad.toLocaleString('en-IN'));
+// ++++++++++++++++++++++ Maths ++++++++++++++++++++
+
+//console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.ceil(6.4))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,6))
+// console.log(Math.max(4,6))
+console.log(Math.random())
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
