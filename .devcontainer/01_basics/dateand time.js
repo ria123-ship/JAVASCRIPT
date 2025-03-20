5d537946a5a940d602fdb1fc0d02710aa9b6f6ad
ria123-ship <riya1singla@gmail.com>
2025-03-20 02:47:03 +0000

    date

---
+++ .devcontainer/01_basics/dateand time.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toDateString);
+// //console.log(myDate.toString);
+// console.log(myDate.toISOString);
+// console.log(typeof myDate);
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toTimeString);
+let mycreatedDate = new Date(2025,0,23,5,3)
+console.log(mycreatedDate.toLocaleString)
