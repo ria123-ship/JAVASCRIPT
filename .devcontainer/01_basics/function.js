f1b1a62a786ed70b9c58f14e587affa8736526db
ria123-ship <riya1singla@gmail.com>
2025-03-22 10:35:12 +0000

    function

---
+++ .devcontainer/01_basics/function.js
@@ -0,0 +1,27 @@
+// function myname(){
+//     console.log("r");
+//     console.log("t");
+//     console.log("y");
+//     console.log("i");
+//     console.log("o");  
+// }
+// //myname()
+// function addtwonum (num1,num2){
+//     //console.log(num1+num2);
+//     return num1 + num2
+
+// }
+
+// const result = addtwonum(2,4)
+//console.log(result)
+function login(username ="sam"){
+    if(username == undefined){
+    console.log("enter username")
+    return 
+}
+    return `${username} just logged in`
+
+}
+ console.log(login("riya"))
+
+
