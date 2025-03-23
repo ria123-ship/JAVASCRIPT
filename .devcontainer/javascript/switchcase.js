681429432ee327a2fa3ec837932821055ce2c9a8
ria123-ship <riya1singla@gmail.com>
2025-03-23 13:46:23 +0000

    controlflow

---
+++ .devcontainer/javascript/switchcase.js
@@ -0,0 +1,33 @@
+// const month= 4
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+
+//     default:
+//         console.log("by default")
+//         break;
+// }
+const month= "march"
+switch (month) {
+    case "jan":
+        console.log("january")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "march":
+        console.log("march")
+        break;
+
+    default:
+        console.log("by default")
+        break;
+}
+
---
+++ .devcontainer/javascript/truthy.js
@@ -0,0 +1,25 @@
+const username =    {} //[] //"riya
+if(username){
+    console.log("got user name");
+    
+}else{
+    console.log("not the email");
+    
+}
+//falsy value
+//false ,0 , -0,BigInt 0n."",undefined,NaN  
+//  truth value
+//truth , {},[],"0",'false',function(){}
+//Nullish coaleasing operator(??) : null undefined
+let val1 ;
+val1=5?? 10
+console.log(val1);
+//terinary operator
+// condition  true:false
+const icetea = 100
+icetea<=80? console.log("less than 80"): console.log("morethan 80");
+
+
+
+
+
