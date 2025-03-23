ff5ccf82409b98cdacbecf100c22fe9f0974cc83
ria123-ship <riya1singla@gmail.com>
2025-03-23 12:25:12 +0000

    controlflow

---
+++ .devcontainer/javascript/control flow.js
@@ -0,0 +1,33 @@
+// if 
+// if(2==2) {
+ // <,> , <=,>= ==, !=,===,!==   
+// }
+// 
+// const score=200 
+// if(score>100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`)
+// }
+// const balance = 1000
+// //if(balance>5000) console.log("test");
+// if (balance<5000){
+//     console.log("lgreater then 5000")
+// } else if(balance<751) {
+//     console.log("less than 750")
+
+// }else{
+//     console.log("less than 1200")
+// }
+// const userlogged = true
+// const debitcard = true
+// const loogedinform =false
+// const loggedinfromemail = true
+// if(userlogged && debitcard){
+//     console.log("allow the user")
+
+// }
+// if(loogedinform || loggedinfromemail){
+//     console.log("userlogged in")
+// }
+
+
